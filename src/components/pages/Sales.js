68b73ddff87c8f93def9b938b1ec11291dbdd6fe
virinarl <vrodriguezl@fi.uba.ar>
2021-07-08 00:23:16 -0300

    Sales page

--- src/components/pages/Sales.js
+++ src/components/pages/Sales.js
@@ -1,8 +1,31 @@
 import React from "react";
-import "./styles/Sales.css";
+import { products } from "../../data/products";
+import ProductItem from "../elements/ProductItem";
+import "./styles/Store.css";
 
 function Sales() {
-  return <h1>Ofertas</h1>;
+  return (
+    <div className="productsList">
+      <h1 className="productList__Title">Available Games On Sale!</h1>
+      <div className="productList__Container">
+        {products.map((product, key) => {
+          return (
+            product.sale === 1 && (
+              <ProductItem
+                url={"/store/" + key}
+                key={key}
+                id={products.id}
+                img={product.img}
+                title={product.title}
+                price={product.price}
+                description={product.description}
+              />
+            )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Sales;
