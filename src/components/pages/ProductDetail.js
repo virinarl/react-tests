6b0241d5e848cff910bb4c31e1565546569ac4ab
virinarl <vrodriguezl@fi.uba.ar>
2021-07-11 22:38:21 -0300

    Remap

--- src/components/pages/ProductDetail.js
+++ src/components/pages/ProductDetail.js
@@ -1,41 +1,67 @@
-import React from "react";
-import { products } from "../../data/products";
+import React, { useState, useEffect } from "react";
+import { getFirestore } from "../../firebase";
 import { useParams } from "react-router-dom";
-import "./styles/ProductDetail.css";
 import AddToCartButton from "../elements/AddToCartButton";
+import { CircularProgress } from "@material-ui/core";
+import "./styles/ProductDetail.css";
 
 function ProductDetail() {
   const { productId } = useParams();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    const db = getFirestore();
+    const gamesCollection = db.collection("games");
+    const productInfo = gamesCollection.where("identificador", "==", productId);
+    productInfo
+      .get()
+      .then((querySnapshot) => {
+        const productInfoDetails = querySnapshot.docs.map((doc) => doc.data());
+        setProducts(productInfoDetails);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [productId]);
 
   return (
     <div className="productDetail">
-      <div
-        className="productDetail__CoverPic"
-        style={{ backgroundImage: `url(${products[productId].bigImg})` }}
-      ></div>
-      <div className="productDetail__info">
-        <h1>{products[productId].title}</h1>
-        <div className="productDetail__infoContainer">
-          <div className="productDetail__infoContainerLeft">
-            <h2>Description</h2>
-            <p>{products[productId].sinopsis}</p>
-            <h2>Requirements</h2>
-            <ul>
-              <li>Operating System: {products[productId].requisitos.SO}</li>
-              <li>Memory: {products[productId].requisitos.memoria}</li>
-              <li>Processor: {products[productId].requisitos.procesador}</li>
-              <li>Grafics: {products[productId].requisitos.graficos}</li>
-            </ul>
-          </div>
-          <div className="productDetail__infoContainerRight">
-            <p>USD${products[productId].price}</p>
-            <AddToCartButton
-              id={products[productId].id}
-              stock={products[productId].stock}
-            />
-          </div>
-        </div>
-      </div>
+      {loading && <CircularProgress />}
+      {products.map((product, key) => {
+        return (
+          <>
+            <div
+              className="productDetail__CoverPic"
+              style={{ backgroundImage: `url(${product.bigImg})` }}
+            ></div>
+            <div className="productDetail__info">
+              <h1>{product.title}</h1>
+              <div className="productDetail__infoContainer">
+                <div className="productDetail__infoContainerLeft">
+                  <h2>Descrition</h2>
+                  <p>{product.sinopsis}</p>
+                  <h2>Requirements</h2>
+                  <ul>
+                    <li>Operating System: {product.requirements.so}</li>
+                    <li>Memory: {product.requirements.memoria}</li>
+                    <li>Processor: {product.requirements.procesador}</li>
+                    <li> Grafics: {product.requirements.graficos}</li>
+                  </ul>
+                </div>
+                <div className="productDetail__infoContainerRight">
+                  <p>USD${product.price}</p>
+                  <AddToCartButton
+                    id={product.identificador}
+                    stock={product.stock}
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
--- src/components/pages/Store.js
+++ src/components/pages/Store.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductItem from "../elements/ProductItem";
-import { products } from "../../data/products";
+import { getFirestore } from "../../firebase";
+import { CircularProgress } from "@material-ui/core";
 import "./styles/Store.css";
 
 function Store() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    setLoading(true);
+    const db = getFirestore();
+    const gamesCollection = db.collection("games");
+    gamesCollection
+      .get()
+      .then((querySnapshot) => {
+        const productsOnSale = querySnapshot.docs.map((doc) => doc.data());
+        setProducts(productsOnSale);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="productsList">
       <h1 className="productList__Title">Available Games</h1>
       <div className="productList__Container">
+        {loading && <CircularProgress />}
         {products.map((product, key) => {
           return (
             <ProductItem
-              url={"/store/" + key}
+              url={"/store/" + product.identificador}
               key={key}
-              id={product.id}
+              id={product.identificador}
               img={product.img}
               title={product.title}
               price={product.price}
